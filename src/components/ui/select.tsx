0119915da650a0a1f620d3590d307d4568a9cf63
cargowolf <cargowolf5@gmail.com>
2024-10-26 15:48:26 +0200

    Update select.tsx

--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -1,3 +1,7 @@
+"use client"
+
+import { cn } from "../../lib/utils"
+
 import * as React from "react"
 import * as SelectPrimitive from "@radix-ui/react-select"
 import { Check, ChevronDown } from "lucide-react"
