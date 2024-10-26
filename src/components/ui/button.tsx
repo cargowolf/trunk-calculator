7d71130066026d120595f9a3a6e1102f61acc3c6
cargowolf <cargowolf5@gmail.com>
2024-10-26 15:46:49 +0200

    Update button.tsx

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,3 +1,7 @@
+"use client"
+
+import { cn } from "../../lib/utils"
+
 import * as React from "react"
 import { Slot } from "@radix-ui/react-slot"
 import { cva, type VariantProps } from "class-variance-authority"
