a136356b4e8a04a3619f5c9c1663612e65532630
cargowolf <cargowolf5@gmail.com>
2024-10-26 15:07:58 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
-import TrunkCalculator from '@/components/trunk-calculator'
-
+import TrunkCalculator from '../components/trunk-calculator'
 export default function Home() {
   return (
     <main className="min-h-screen p-4">
