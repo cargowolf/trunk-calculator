f3cff7b0227b25fbebc2ad006ef1d0bd8912419f
cargowolf <cargowolf5@gmail.com>
2024-10-26 13:50:16 +0200

    Add home page component

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import TrunkCalculator from '@/components/trunk-calculator'
+
+export default function Home() {
+  return (
+    <main className="min-h-screen p-4">
+      <TrunkCalculator />
+    </main>
+  )
+}
